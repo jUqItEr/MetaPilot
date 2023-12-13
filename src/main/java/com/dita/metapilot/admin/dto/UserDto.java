15404d94107272aed10c603137a75926760db2d0
hs <xoghwkd6505@gmail.com>
2023-12-13 10:02:27 +0900

    Admin - 유저 차단 관련

--- src/main/java/com/dita/metapilot/admin/dto/UserDto.java
+++ src/main/java/com/dita/metapilot/admin/dto/UserDto.java
@@ -1,5 +1,12 @@
 package com.dita.metapilot.admin.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
-    String userId;
+    private String userId;
 }
