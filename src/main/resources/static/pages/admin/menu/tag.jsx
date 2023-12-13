6c6bd2d6d662473ed29fe9c0647a6552cbb6f0ef
gommzzy <gommzzy@naver.com>
2023-12-13 17:35:37 +0900

    Hashtag 부분 수정

--- src/main/resources/static/pages/admin/menu/tag.jsx
+++ src/main/resources/static/pages/admin/menu/tag.jsx
@@ -3,9 +3,10 @@ import Image from 'next/image'
 import axios from 'axios'
 import $ from 'jquery'
 import styles from '../../../styles/admin/menu/tag.module.css'
-import {useEffect} from "react"
+import {useEffect,useState} from "react"
 import AdminHeader from "../../../layout/admin/header"
 import AdminSidebar from "../../../layout/admin/sidebar"
+
 /**
  * Rendering the blog info page
  *
@@ -13,11 +14,76 @@ import AdminSidebar from "../../../layout/admin/sidebar"
  * @since 2023. 12. 07.
  * @returns
  */
+
 export default function AdminHashTagPage() {
+    const [tag, setTag] = useState([]);
+    const [originalTag, setOriginalTag] = useState()
+
+    const changeTag = component => {
+        axios({
+            method: 'post',
+            params: {
+                content: component
+            },
+            url: '/api/admin/'
+        })
+    }
+
+    const moveTag = component => {
+        const tag = component.content
+        $('#tag-name').val(tag)
+        $('#tag-name').attr('readOnly', false)
+        setOriginalTag(tag)
+    }
+
+    const modifyTag = () => {
+        const tag = $('#tag-name').val()
+        const isInitial = originalTag === tag
+
+        if (!isInitial) {
+            axios({
+                method: 'get',
+                params: {
+                    content: $('#tag-name').val()
+                },
+                url: '/api/admin/hasTag'
+            })
+            .then((res) => {
+                if (!res.data) {
+                    changeTag(tag)
+                } else {
+                    alert('이미 사용 중인 해시태그입니다.')
+                }
+            })
+        } else {
+            alert('해시태그를 수정하려면 변경해주세요.')
+        }
+    }
+
+    const removeTag = () => {
+
+    }
+    
+    useEffect(() => {        
+        axios({
+            method: 'get',
+            params: {
+                type: 0
+            },
+            url: '/api/admin/readTag'
+        })
+        .then((res) => {
+            setTag(res.data)
+            console.log("here : ",res)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }, []);
+
     return(
         <>
-            <Head>
-                {/*        {`${styles. }  `}       */}
+            <Head> 
                 <title>관리자 페이지</title>
                 <meta property='og:title' content='관리자 페이지' key='title'/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -35,18 +101,12 @@ export default function AdminHashTagPage() {
                             <div className={styles.listField}>
                                 <div  style={{maxHeight:'400px', overflowY: 'auto', width:'350px'}}>
                                     <div className="list-group">
-                                        <button type="button" className="list-group-item list-group-item-action ">#hello(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#hi(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#memo(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#hello(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#hi(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#memo(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#hello(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#hi(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#memo(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#hello(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#hi(1)</button>
-                                        <button type="button" className="list-group-item list-group-item-action">#memo(1)</button>
+                                        {
+                                            tag?.map((mapper) => (
+                                                <button className="list-group-item list-group-item-action" type="button"
+                                                    key={mapper.content} onClick={() => moveTag(mapper)}>{mapper.content}</button>
+                                            ))
+                                        }
                                     </div>
                                 </div>
                             </div>
@@ -58,10 +118,10 @@ export default function AdminHashTagPage() {
 
                                 </div>
                                 <label className="pb-3 mt-5">태그 수정, 삭제</label>
-                                <div className="input-group ">
-                                    <input type="text" className="form-control" placeholder="수정할 해시태그명을 입력하세요."   aria-describedby="button-addon2"/>
-                                    <button className="btn btn-primary ps-3 pe-3" type="button" id="btnUpdate" style={{border:'1px solid white'}}>수정</button>
-                                    <button className="btn btn-primary ps-3 pe-3" type="button" id="btnDelete" style={{border:'1px solid white'}}>삭제</button>
+                                <div className="input-group">
+                                    <input className="form-control" id="tag-name" type="text" placeholder="수정할 해시태그명을 입력하세요." aria-describedby="button-addon2" readOnly/>
+                                    <button className="btn btn-primary ps-3 pe-3" type="button" id="btnUpdate" style={{border:'1px solid white'}} onClick={modifyTag}>수정</button>
+                                    <button className="btn btn-primary ps-3 pe-3" type="button" id="btnDelete" style={{border:'1px solid white'}} onClick={removeTag}>삭제</button>
                                 </div>
                             </div>
                         </div>
